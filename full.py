6a4ea2db9268c554024bd426052560b4b9afaab2
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2021-07-08 21:00:21 -0700

    main

--- full.py
+++ full.py
@@ -1,17 +1,24 @@
-import keyboard
+import ctypes
 import os
 import time
 
-delay = 1
+# Virtual-Key Code for Pause and Play Media Key "https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes"
+VK_MEDIA_PLAY_PAUSE = 0xB3
+
+delay = 1  # Time Between Window Check
+# Loading User32.dll for the Media Keys
+dll_user32 = ctypes.WinDLL("user32.dll")
 running_seconds, running_minutes, stopped_seconds, stopped_minutes = 0, 0, 0, 0
 
 while True:
+    # poop code below
     check_if_running = os.popen(
         "tasklist /NH /FO:CSV /FI \"IMAGENAME eq Spotify.exe\"").read()
     check_if_ad_exist = os.popen(
         "tasklist /NH /FO:CSV /FI \"WINDOWTITLE eq Advertisement\"").read()
     check_if_another_ad_exist = os.popen(
         "tasklist /NH /FO:CSV /FI \"WINDOWTITLE eq Spotify\"").read()
+    # poop code above
 
     if "Spotify.exe" in check_if_ad_exist or "Spotify.exe" in check_if_another_ad_exist:
         print("###########################")
@@ -21,13 +28,13 @@ while True:
         print("Waiting 1 Seconds for Spotify to Launch")
         time.sleep(1)
         os.system(
-            "start /MIN C:\\Users\\{}\\AppData\\Roaming\\Spotify\\Spotify.exe".format(os.getlogin()))
+            "powershell Start-Process {}\Spotify\Spotify.exe -WindowStyle Minimized".format(os.getenv('appdata')))
         print("Spotify Launched")
         time.sleep(0.5)
-        keyboard.press_and_release('play/pause media')
+        # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-keybd_event
+        dll_user32.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, 1, 0)
         print("Music Started")
         print("###########################")
-        running_seconds, running_minutes = 0, 0
 
     elif "Spotify.exe" in check_if_running:
         running_seconds += 1
--- minimal + no_console.pyw
+++ minimal + no_console.pyw
@@ -1,23 +1,31 @@
-import keyboard
+import ctypes
 import os
 import time
 
-delay = 1
+# Virtual-Key Code for Pause and Play Media Key "https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes"
+VK_MEDIA_PLAY_PAUSE = 0xB3
+
+delay = 1  # Time Between Window Check
+# Loading User32.dll for the Media Keys
+dll_user32 = ctypes.WinDLL("user32.dll")
 
 while True:
+    # poop code below
     check_if_running = os.popen(
         "tasklist /NH /FO:CSV /FI \"IMAGENAME eq Spotify.exe\"").read()
     check_if_ad_exist = os.popen(
         "tasklist /NH /FO:CSV /FI \"WINDOWTITLE eq Advertisement\"").read()
     check_if_another_ad_exist = os.popen(
         "tasklist /NH /FO:CSV /FI \"WINDOWTITLE eq Spotify\"").read()
+    # poop code above
 
     if "Spotify.exe" in check_if_ad_exist or "Spotify.exe" in check_if_another_ad_exist:
         os.system("taskkill /IM Spotify.exe")
         time.sleep(1)
         os.system(
-            "start /MIN C:\\Users\\{}\\AppData\\Roaming\\Spotify\\Spotify.exe".format(os.getlogin()))
+            "powershell Start-Process {}\Spotify\Spotify.exe -WindowStyle Minimized".format(os.getenv('appdata')))
         time.sleep(0.5)
-        keyboard.press_and_release('play/pause media')
+        # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-keybd_event
+        dll_user32.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, 1, 0)
 
     time.sleep(float(delay))
